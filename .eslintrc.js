5da481665434decaf5828c26c5bc33770dc90203
Salmu10 <Ubedasalmu@gmail.com>
2022-12-12 19:46:38 +0100

    Linter test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es2021": true
-  },
-  "parserOptions": {
-      "ecmaVersion": 12,
-      "sourceType": "module"
-  },
-  "rules": {
-  }
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
